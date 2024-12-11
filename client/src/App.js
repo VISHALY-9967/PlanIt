868899bfb0030baca65a33a7b4af275721cedb7b
Vishal_Yadav <vishalyadav91665@gmail.com>
2024-12-12 01:26:04 +0530

    final setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import Login from './pages/Login';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                {/* <Route path="/" element={<Home />} /> */}
-                {/* <Route path="/login" element={<Login />} /> */}
-            </Routes>
-        </Router>
+        <h1>Hello World</h1>
     );
 }
 
